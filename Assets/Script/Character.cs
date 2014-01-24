c582cfb1552e1aeb7fac6ef28a63b9a2ffdd9130
spoki0 <spoki0@hotmail.com>
2014-01-24 23:12:29 +0100

    moar scripts

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	int userID;
+	int lane;
+	int speed;
+	Color colour;
+		
+
+	// Use this for initialization
+	void Start (int _id) {
+		userID = _id;
+		lane = _id;
+		//speed = Main.ultimateSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//movement
+
+	}
+}
