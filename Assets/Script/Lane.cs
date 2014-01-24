7db4d8102b9bb16204e670dc075f1f66a8b08d38
FreddyNO <fredhno@gmail.com>
2014-01-24 23:10:55 +0100

    La til masse classes yay

---
+++ Assets/Script/Lane.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lane : MonoBehaviour {
+	
+	public Rigidbody[] arrayObjects;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("SpawnObject");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	IEnumerator SpawnObject(){
+		yield return new WaitForSeconds(10);
+		int randomSpawn = Random.Range(0,2);
+		
+		if(randomSpawn == 0){
+			randomSpawn = Random.Range(0,1);
+			arrayObjects[randomSpawn].gameObject = new GameObject();
+		}
+	}
+}
