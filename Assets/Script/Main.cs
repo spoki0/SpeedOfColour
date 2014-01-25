3ddb456a65dc3fd9a14ed740f5172eecc5b99ebc
FreddyNO <fredhno@gmail.com>
2014-01-25 15:39:57 +0100

    statisk liste

--- Assets/Script/Main.cs
+++ Assets/Script/Main.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class Main : MonoBehaviour {
 	
 	public static float ultimateSpeed = -10;
-	public List<Color> listColours = new List<Color>();
+	public static List<Color> listColours = new List<Color>();
 	public GameObject stage;
 	int randomCInt;
 	
@@ -31,6 +31,7 @@ public class Main : MonoBehaviour {
 			randomColour.g = 1;
 		if(randomCInt == 2)
 			randomColour.b = 1;
+		
 		
 		
 		Instantiate(stage, new Vector3(0,0,0), stage.transform.rotation);
