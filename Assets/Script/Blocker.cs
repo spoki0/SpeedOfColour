9829c6daa993c0f836f1c006657f82ac12d787f7
spoki0 <spoki0@hotmail.com>
2014-01-25 19:21:28 +0100

    Fix

--- Assets/Script/Blocker.cs
+++ Assets/Script/Blocker.cs
@@ -17,7 +17,7 @@ public class Blocker : Object {
 	void OnCollisionEnter(Collision col){
 		if(col.gameObject.tag == "Player"){
 
-			if(transform.renderer.material.color != Color(0.5, 0.5, 0.5)){
+			if(transform.renderer.material.color != new Color(0.5f, 0.5f, 0.5f)){
 
 				bool valid = true;
 				if (col.transform.renderer.material.color.r != 0){
